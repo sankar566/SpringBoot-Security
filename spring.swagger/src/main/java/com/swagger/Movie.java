dd3508eaaa02155440906aa9eb8a83242b002be4
sankar566 <60536375+sankar566@users.noreply.github.com>
2021-01-20 16:09:34 +0530

    Add files via upload

---
+++ spring.swagger/src/main/java/com/swagger/Movie.java
@@ -0,0 +1,21 @@
+package com.swagger;
+
+public class Movie {
+	private String name;
+	private String actor;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getActor() {
+		return actor;
+	}
+	public void setActor(String actor) {
+		this.actor = actor;
+	}
+	
+	
+
+}
