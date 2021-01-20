ef282aa2183f53782acc439e09f6d460b38d6852
sankar566 <60536375+sankar566@users.noreply.github.com>
2021-01-20 16:05:34 +0530

    Add files via upload

---
+++ spring.security/src/main/java/com/security/HelloController.java
@@ -0,0 +1,16 @@
+package com.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@GetMapping
+	public String sayhello() {
+		return "hello world";
+	}
+
+}
